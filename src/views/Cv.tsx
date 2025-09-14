33d79228f7f1883539b2d0c145f9713efa71ee79
Sandra Paläng <sandra.palang@gmail.com>
2025-09-14 11:24:55 +0200

    feat(view): add cv view skeleton and mount in app layout

---
+++ src/views/Cv.tsx
@@ -0,0 +1,19 @@
+function Cv() {
+	return (
+		<>
+			<div className="cv-container">
+				<div className="cv-title">
+					<h2>arbete</h2>
+				</div>
+			</div>
+
+			<div className="cv-container">
+				<div className="cv-title">
+					<h2>utbildning</h2>
+				</div>
+			</div>
+		</>
+	)
+}
+
+export default Cv
