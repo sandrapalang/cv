43868091e0bff6f25121482392a0d9af52795174
Sandra Paläng <sandra.palang@gmail.com>
2025-10-01 15:46:58 +0200

    chore(copy): use fuller placeholder copy for realistic rendering

--- src/data/data.ts
+++ src/data/data.ts
@@ -13,8 +13,11 @@ export const data: CvData = {
 					'https://www.iths.se/utbildningar/frontend/?_gl=1*1qa4iwp*_up*MQ..&gclid=CjwKCAjw2dG1BhB4EiwA998cqD-7SgzDc-Ke2CNO6DjM6ikmwTAEkw-RP5MlmETYu_DpZj5lW5hhVRoCi2cQAvD_BwE',
 				linkText: 'iths.se',
 			},
-			content: ['Lorem ipsum dolor sit amet…', 'Lorem ipsum dolor sit amet…'],
 			skills: ['HTML', 'CSS', 'JavaScript', 'TypeScript', 'React', 'Vue'],
+			content: [
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam. Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam. Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
+			],
 		},
 		{
 			id: 'WEB1',
@@ -26,8 +29,11 @@ export const data: CvData = {
 				linkUrl: 'https://hermods.se/utbildningar/webbutveckling-1/',
 				linkText: 'hermods.se',
 			},
-			content: ['Lorem ipsum dolor sit amet…', 'Lorem ipsum dolor sit amet…'],
 			skills: ['HTML', 'CSS', 'Php', 'WordPress'],
+			content: [
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam. Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam. Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
+			],
 		},
 		{
 			id: 'PRR1',
@@ -39,8 +45,11 @@ export const data: CvData = {
 				linkUrl: 'https://hermods.se/utbildningar/programmering-1/',
 				linkText: 'hermods.se',
 			},
-			content: ['Lorem ipsum dolor sit amet…', 'Lorem ipsum dolor sit amet…'],
 			skills: ['C#', 'Python', 'Visual Studio', 'Git'],
+			content: [
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam. Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam. Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
+			],
 		},
 	],
 	headings: {
@@ -259,8 +268,11 @@ export const data: CvData = {
 				linkUrl: 'https://www.stendahls.se',
 				linkText: 'stendahls.se',
 			},
-			content: ['Lorem ipsum dolor sit amet…', 'Lorem ipsum dolor sit amet…'],
 			skills: ['JavaScript', 'React', 'TypeScript', 'HTML', 'CSS'],
+			content: [
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam. Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam. Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
+			],
 		},
 		{
 			id: 'LIA1',
@@ -272,8 +284,11 @@ export const data: CvData = {
 				linkUrl: 'https://www.stendahls.se',
 				linkText: 'stendahls.se',
 			},
-			content: ['Lorem ipsum dolor sit amet…', 'Lorem ipsum dolor sit amet…'],
 			skills: ['JavaScript', 'Vue', 'TypeScript', 'HTML', 'CSS'],
+			content: [
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam. Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam. Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
+			],
 		},
 	],
 }
